44f5bfa0db462b81fb3a579489f742d25dcee43b
cf7 <cfranco7@lion.lmu.edu>
2017-04-04 17:10:08 -0700

    notes from class

--- test/generic_api_test.js
+++ test/generic_api_test.js
@@ -10,3 +10,24 @@ describe('GET /', () => {
       .expect(404, { message: 'Nothing to see here' }, done);
   });
 });
+
+
+/*
+
+  SQL Notes (this might be in the final):
+
+  "inner join" or "join" - only rows that match with each other will appear in the result table
+  "left join" - all rows from left table will appear regardless if they don't have a match in the right table
+                (those without matches will have a NULL on the right column of the result table)
+  "full join" - all rows from both the left and right table will appear in the result table
+                regardless if they have matches or not
+
+
+  Example:
+    What is the result of this query?
+    SELECT s."firstName" as staff, r.name AS role
+    FROM "StaffMember" s
+    LEFT JOIN "StaffMemberRole" sr on sr."staffMemberId"=s.id
+    LEFT JOIN "Role" r on sr."roleId"=r.id;
+
+*/
