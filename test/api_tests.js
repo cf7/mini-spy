c6be15099d10b2e46b5e624b26a4180066a9342d
cf7 <cfranco7@lion.lmu.edu>
2017-04-13 04:13:26 -0700

    update test formats

--- test/api_tests.js
+++ test/api_tests.js
@@ -2,8 +2,8 @@ const app = require('../app');
 const expect = require('chai').expect;
 const request = require('supertest');
 
-function postRequest(name, description) {
-  return request(app).post('/roles').send({ name, description });
+function postRequest(url, payload) {
+  return request(app).post(url).send(payload);
 }
 
 describe('GET /', () => {
@@ -38,22 +38,29 @@ describe('API Role Endpoints', () => {
   });
 
   it('gets all roles', (done) => {
-    postRequest('administator', 'administrates').expect(201)
-      .then(() => postRequest('staff', 'staffs').expect(201))
-      .then(() => postRequest('volunteer', 'volunteers').expect(201))
+    let url = '/roles';
+    postRequest(url, { name: 'administator', description: 'administrates' }).expect(201)
+      .then(() => postRequest(url, { name: 'staff', description: 'staffs' }).expect(201))
+      .then(() => postRequest(url, { name: 'volunteer', description: 'volunteers' }).expect(201))
       .then(() => request(app).get('/roles').expect(200))
       .then((response) => {
         expect(response.body.length).to.equal(3);
-        expect(response.body[0]).to.have.property('name');
         expect(response.body[0].name).to.equal('administator');
-        expect(response.body[0]).to.have.property('description');
         expect(response.body[0].description).to.equal('administrates');
+        expect(response.body[1].name).to.equal('staff');
+        expect(response.body[1].description).to.equal('staffs');
+        expect(response.body[2].name).to.equal('volunteer');
+        expect(response.body[2].description).to.equal('volunteers');
         return done();
       });
   });
 });
 
 describe('API StaffMember Endpoints', () => {
+  beforeEach((done) => {
+    request(app).delete('/staffmembers').end(done);
+  });
+
   it('creates a new staffmember account', (done) => {
     request(app)
       .post('/staffmembers')
@@ -71,38 +78,27 @@ describe('API StaffMember Endpoints', () => {
         return done();
       });
   });
-  it('creates a new staffmember account', (done) => {
-    request(app)
-      .post('/staffmembers')
-      .send({
-        username: 'staff456',
-        password: 'thisisapasswordagain',
-        firstName: 'Ted',
-        lastName: 'Greene',
-      })
-      .expect(201)
-      .end((error, response) => {
-        if (error) {
-          return done(error);
-        }
-        return done();
-      });
-  });
 
   it('gets all staffmember account information', (done) => {
-    request(app)
-      .get('/staffmembers')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((error, response) => {
-        if (error) {
-          return done(error);
-        }
-        expect(response.body).to.have.property('firstName');
-        expect(response.body.name).to.equal('Carrie');
-        expect(response.body).to.have.property('lastName');
-        expect(response.body.description).to.equal('Smith');
+    let url = '/staffmembers';
+    postRequest(url, { username: 'staff123', password: 'thisisapassword', firstName: 'Carrie', lastName: 'Smith' }).expect(201)
+      .then(() => postRequest(url, { username: 'newstaff', password: 'thisisapassword1', firstName: 'Kathy', lastName: 'Jones' }).expect(201))
+      .then(() => postRequest(url, { username: 'staffer', password: 'thisisapassword2', firstName: 'Ted', lastName: 'Greene' }).expect(201))
+      .then(() => request(app).get('/roles').expect(200))
+      .then((response) => {
+        expect(response.body.length).to.equal(3);
+        expect(response.body[0].username).to.have.property('staff123');
+        expect(response.body[0].password).to.equal('thisisapassword');
+        expect(response.body[0].firstName).to.have.property('Carrie');
+        expect(response.body[0].lastName).to.equal('Smith');
+        expect(response.body[1].username).to.have.property('newstaff');
+        expect(response.body[1].password).to.equal('thisisapassword1');
+        expect(response.body[1].firstName).to.have.property('Kathy');
+        expect(response.body[1].lastName).to.equal('Jones');
+        expect(response.body[2].username).to.have.property('staffer');
+        expect(response.body[2].password).to.equal('thisisapassword2');
+        expect(response.body[2].firstName).to.have.property('Ted');
+        expect(response.body[2].lastName).to.equal('Greene');
         return done();
       });
   });
